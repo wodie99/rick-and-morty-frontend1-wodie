0621b0123e075191a6a2aa85c9efb57aefc601e0
LBarenschee <barenschee@gmail.com>
2022-04-14 11:45:10 +0200

    added more values to CharacterCard, styled CharacterCard, looks fancy

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,4 +1,5 @@
 import {Character} from "../model/Character";
+import "./CharacterCard.css";
 
 type CharacterCardProps = {
     character : Character
@@ -7,5 +8,8 @@ type CharacterCardProps = {
 export default function CharacterCard( { character } : CharacterCardProps ) {
     return <div className = {"character-card"}>
         <h2> {character.name} </h2>
+        <img src={character.image}/>
+        <h3> Status: {character.status}</h3>
+        <h4> Origin: {character.origin.name}</h4>
     </div>
 }
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,12 +1,13 @@
 import {Character} from "../model/Character";
 import CharacterCard from "./CharacterCard";
+import "./Gallery.css";
 
 type GalleryProps = {
      characters: Character[]
 }
 
 export default function Gallery({characters}:GalleryProps){
-     return  <div>{characters.map(character => <CharacterCard character = {character} />)
+     return  <div className={"character-gallery"}>{characters.map(character => <CharacterCard character = {character} />)
 
      } </div>
 }
