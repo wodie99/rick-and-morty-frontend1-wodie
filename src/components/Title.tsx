1907b561382ba5ed89783988b735d9f90e193730
wodie99 <wodie99@wodie.net>
2022-04-18 22:49:15 +0200

    added pagination

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,9 @@
 import "./Title.css"
 
-export default function Title(){
-    return <div className="title-block">
-                <img id={"rm-logo"} src={"https://rickandmortyapi.com/icons/icon-512x512.png"} alt={"Rick and Morty Logo"} />
-                <h1>Rick & Morty Character Gallery</h1>
-            </div>
 
+export default function Title() {
+    return <div className="title-block">
+        <img id={"rm-logo"} src={"https://rickandmortyapi.com/icons/icon-512x512.png"} alt={"Rick and Morty Logo"}/>
+        <h1>Rick & Morty Character Gallery</h1>
+    </div>
 }
